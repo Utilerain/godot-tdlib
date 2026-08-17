25eb0ccd9f2d838a5820cc54844d0c8840f70b80
UtileRain <utilerainpaw@gmail.com>
2026-08-17 23:42:06 +0500

    fix generator

--- scripts/generate_docs.ps1
+++ scripts/generate_docs.ps1
@@ -1,3 +1,6 @@
 # .\gen\godot.exe - path to godot executable
-.\gen\godot.exe --path ./example --doctool ./ --gdextension-docs
-rm ./example/doc_classes
+Start-Process -FilePath ".\gen\godot.exe" -ArgumentList "--path ./example --doctool ./ --gdextension-docs" -Wait -NoNewWindow
+
+if (Test-Path "./example/doc_classes") {
+    Remove-Item -Force -Recurse "./example/doc_classes"
+}
