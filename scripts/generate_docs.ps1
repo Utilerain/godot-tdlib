28c99583099211a6d75a310aff8113109ebb8703
UtileRain <utilerainpaw@gmail.com>
2026-08-17 23:24:18 +0500

    update documentation

--- scripts/generate_docs.ps1
+++ scripts/generate_docs.ps1
@@ -1,2 +1,3 @@
 # .\gen\godot.exe - path to godot executable
-.\gen\godot.exe --path .\example --doctool .\ --gdextension-docs
+.\gen\godot.exe --path ./example --doctool ./ --gdextension-docs
+rm ./example/doc_classes
