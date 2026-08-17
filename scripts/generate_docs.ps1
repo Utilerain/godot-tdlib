bbbfc323435c92e77f7122ff5d0f15a6784bf361
UtileRain <utilerainpaw@gmail.com>
2026-08-18 00:12:02 +0500

    fix generator

--- scripts/generate_docs.ps1
+++ scripts/generate_docs.ps1
@@ -1,6 +1,7 @@
 # .\gen\godot.exe - path to godot executable
-Start-Process -FilePath ".\gen\godot.exe" -ArgumentList "--path ./example --doctool ./ --gdextension-docs" -Wait -NoNewWindow
+$RootDir = Resolve-Path "$PSScriptRoot\.."
 
-if (Test-Path "./example/doc_classes") {
-    Remove-Item -Force -Recurse "./example/doc_classes"
-}
+Start-Process -FilePath ".\gen\godot.exe" `
+    -ArgumentList "--path ./example --doctool ""$RootDir"" --gdextension-docs" `
+    -Wait `
+    -NoNewWindow
