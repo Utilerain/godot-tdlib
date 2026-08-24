b2799b0a59e2cb7386ae991ad5bace8ebd0ac867
UtileRain <utilerainpaw@gmail.com>
2026-08-25 01:35:32 +0500

    update commit hash for build

--- thirdparty/build_android.sh
+++ thirdparty/build_android.sh
@@ -1,6 +1,6 @@
 cd tdlib_android
 sudo apt-get install -y unzip
-commit_hash=1b08c83bc07888e4b0a6150d36c1364ff03cf930
+commit_hash=d1085f9cebc5a62379991ae1652673954f229c1f
 docker build --build-arg TDLIB_INTERFACE=JSON --build-arg ANDROID_STL=c++_shared --build-arg ANDROID_NDK_VERSION=28.1.13356709 --build-arg COMMIT_HASH=$commit_hash --output builds .
 cd builds
 unzip tdlib.zip
