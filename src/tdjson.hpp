eb28e73aa21aa54a0524d7271ffb4f96277034f2
UtileRain <utilerainpaw@gmail.com>
2026-09-15 19:38:06 +0500

    refactor: naming parameters in tdjson.hpp

--- src/tdjson.hpp
+++ src/tdjson.hpp
@@ -22,18 +22,18 @@ namespace godot
         void set_log_callback(Callable callback);
         int get_client_id();
         void set_tdlib_parameters(
-            int api_id,
-            String api_hash,
-            String application_version,
-            String device_model,
-            String database_directory = String("user://tdlib_data"),
-            bool use_test_dc = false,
-            String files_directory = String(""),
-            bool use_file_database = true,
-            bool use_message_database = true,
-            bool use_secret_chats = true,
-            String system_language_code = String(""),
-            String system_version = String(""));
+            int p_api_id,
+            String p_api_hash,
+            String p_application_version,
+            String p_device_model,
+            String p_database_directory = String("user://tdlib_data"),
+            bool p_use_test_dc = false,
+            String p_files_directory = String(""),
+            bool p_use_file_database = true,
+            bool p_use_message_database = true,
+            bool p_use_secret_chats = true,
+            String p_system_language_code = String(""),
+            String p_system_version = String(""));
         String get_tdlib_version();
         void start_poll();
         void stop_poll();
